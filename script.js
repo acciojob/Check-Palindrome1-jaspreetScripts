e51fdcf8ea32d05352908ed356bab0f24509cc85
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-17 15:50:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+	for( let i=0;i<str.length;i++){
+		let j=str.length-1;
+		if( i<j && str[i]!==str[j]){
+			return false;
+		}
+	}
+	return true;
 
 }
 module.exports = palindrome
