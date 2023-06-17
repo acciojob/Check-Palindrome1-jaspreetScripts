622db6efdc99bc6c2e1160961f995f8a4eff4095
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-17 15:56:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-	for( let i=0;i<str.length;i++){
 		let j=str.length-1;
+	for( let i=0;i<str.length;i++){
 		if( i<=j && str[i]!==str[j]){
 			return false;
 		}
